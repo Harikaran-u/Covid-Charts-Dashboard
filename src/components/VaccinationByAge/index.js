7f5f23ec4061bd34fe278a4e44b0aa5850473806
Harikaran-u <sanjuhari971@gmail.com>
2023-07-22 07:53:10 +0000

    Learn React Charts

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -1,4 +1,4 @@
-import {PieChart, Pie, Legend, Cell, ResponsiveContainer} from 'recharts'
+import {PieChart, Pie, Legend, Cell} from 'recharts'
 
 import './index.css'
 
@@ -8,32 +8,30 @@ const VaccinationByAge = props => {
   const chartAge = (
     <div className="age-bg-cont">
       <h1 className="age-head">Vaccination by Age</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="50%"
-            cy="50%"
-            data={age}
-            startAngle={0}
-            endAngle={360}
-            innerRadius="0%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="18-44" fill="#5a8dee" />
-            <Cell name="45-60" fill="#a3df9f" />
-            <Cell name="Above 60" fill="#2cc6c6" />
-          </Pie>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="50%"
+          cy="50%"
+          data={age}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="0%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#5a8dee" />
+          <Cell name="45-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#2cc6c6" />
+        </Pie>
 
-          <Legend
-            iconType="circle"
-            align="center"
-            wrapperStyle={{
-              padding: 30,
-            }}
-          />
-        </PieChart>
-      </ResponsiveContainer>
+        <Legend
+          iconType="circle"
+          align="center"
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+      </PieChart>
     </div>
   )
   return chartAge
--- src/components/VaccinationCoverage/index.js
+++ src/components/VaccinationCoverage/index.js
@@ -1,11 +1,4 @@
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  Legend,
-  ResponsiveContainer,
-} from 'recharts'
+import {BarChart, Bar, XAxis, YAxis, Legend} from 'recharts'
 
 import './index.css'
 
@@ -22,31 +15,30 @@ const VaccinationCoverage = props => {
   const chart = (
     <div className="chart-bg-cont">
       <h1 className="bar-title">Vaccination Coverage</h1>
-      <ResponsiveContainer width="100%" height={500}>
-        <BarChart width={1000} height={300} data={last7DaysVaccination}>
-          <XAxis
-            dataKey="vaccine_date"
-            tick={{
-              stroke: 'gray',
-              strokeWidth: 1,
-            }}
-          />
-          <YAxis
-            tickFormatter={DataFormatter}
-            tick={{
-              stroke: 'gray',
-              strokeWidth: 0,
-            }}
-          />
-          <Legend
-            wrapperStyle={{
-              padding: 30,
-            }}
-          />
-          <Bar dataKey="dose_1" name="Dose 1" fill="#5a8dee" barSize="20%" />
-          <Bar dataKey="dose_2" name="Dose 2" fill="#f54394" barSize="20%" />
-        </BarChart>
-      </ResponsiveContainer>
+
+      <BarChart width={1000} height={300} data={last7DaysVaccination}>
+        <XAxis
+          dataKey="vaccine_date"
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 1,
+          }}
+        />
+        <YAxis
+          tickFormatter={DataFormatter}
+          tick={{
+            stroke: 'gray',
+            strokeWidth: 0,
+          }}
+        />
+        <Legend
+          wrapperStyle={{
+            padding: 30,
+          }}
+        />
+        <Bar dataKey="dose_1" name="Dose 1" fill="#5a8dee" barSize="20%" />
+        <Bar dataKey="dose_2" name="Dose 2" fill="#f54394" barSize="20%" />
+      </BarChart>
     </div>
   )
 
